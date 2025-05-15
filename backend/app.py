eab06a25e933e43ba8f1c14fbf41e84a8172678d
Muhammad Mohib Ali <138523563+mohibali082@users.noreply.github.com>
2025-05-15 05:44:15 +0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS, cross_origin
 from models import db, User
 from config import ApplicationConfig
 from flask_session import Session
-#the all changes are made
+#comment
 
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
